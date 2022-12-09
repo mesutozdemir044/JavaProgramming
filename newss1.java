777c4020f97c31419a6efdb1069b3c9e1c4c82aa
Mesut Ozdemir <mesutozdemir044@gmail.com>
2022-12-09 17:17:30 +0300

    ilk versiyon

---
+++ newss1.java
@@ -0,0 +1,9 @@
+public class newss1 {
+    public static void main(String[] args) {
+        System.out.println("mesut" +
+                "rr");
+    }
+    newss1 new2=new newss1();
+    new2.add("4");
+
+}
---
+++ src/githup/gtpp.java
@@ -0,0 +1,7 @@
+package githup;
+
+public class gtpp {
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
